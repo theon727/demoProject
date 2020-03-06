11c7bba31641e4f777c2e914c24febb7fb6740aa
ux016435 <ux016435@TP6BVDIW7DV0046.ten.thomsonreuters.com>
2020-03-06 10:04:13 -0500

    Add a comment

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2Application {
 
+	//Test comment
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
